351d9eed9b8982c3dd4fffee50ca79e6a99c732f
shresthkapoor7 <73338689+shresthkapoor7@users.noreply.github.com>
2021-09-16 17:37:37 +0530

    Stub Project

---
+++ lib/screens/location_screen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:clima/utilities/constants.dart';
+
+class LocationScreen extends StatefulWidget {
+  @override
+  _LocationScreenState createState() => _LocationScreenState();
+}
+
+class _LocationScreenState extends State<LocationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 50,
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(
+            Icons.near_me,
+            size: 30,
+          ),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.location_city, size: 30), onPressed: () {}),
+        ],
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/location_background.jpg'),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+                Colors.white.withOpacity(0.8), BlendMode.dstATop),
+          ),
+        ),
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    '32°',
+                    style: kTempTextStyle,
+                  ),
+                  Text(
+                    '☀️',
+                    style: kConditionTextStyle,
+                  ),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 20.0, right: 20),
+                child: Text(
+                  "It's 🍦 time in San Francisco!",
+                  textAlign: TextAlign.center,
+                  style: kMessageTextStyle,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
